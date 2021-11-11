9fd6a79fad08d5fe376ccd665ebb6e93d9bf1480
(hyebin) <(sksp4334@naver.com)>
2021-11-11 23:18:53 +0900

    useState연습하기

--- src/core/react.js
+++ src/core/react.js
@@ -1,8 +1,36 @@
+let state = undefined;
+
 function useState(initState) {
-  let state = initState;
+  if (state === undefined) {
+    state = initState;
+  }
   const setState = (newState) => {
     state = newState;
     render();
   };
   return [state, setState];
 }
+
+function Counter() {
+  const [count, setCount] = useState(1);
+  window.increment = () => {
+    setCount(count + 1);
+  };
+
+  return `
+    <div>
+      <h3>count: ${count} </h3>
+      <button onclick="increment()">증가</button>
+    </div>
+  `;
+}
+
+function render() {
+  const app = document.querySelector("#app");
+  app.innerHTML = `
+    <div>
+      ${Counter()}
+    </div>
+  `;
+}
+render();
